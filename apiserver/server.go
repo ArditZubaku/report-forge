ed59365b99f1d1f40a3f37073c1d46be15b48ecc
Ardit Zubaku <zubaku92@gmail.com>
2026-09-09 09:07:56 +0200

    Add apiserver with ping endpoint and graceful shutdown

---
+++ apiserver/server.go
@@ -0,0 +1,64 @@
+package apiserver
+
+import (
+	"context"
+	"errors"
+	"log"
+	"log/slog"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/ArditZubaku/async-api/config"
+)
+
+type ApiServer struct {
+	config *config.Config
+	logger *slog.Logger
+}
+
+func New(config *config.Config, logger *slog.Logger) *ApiServer {
+	return &ApiServer{config: config, logger: logger}
+}
+
+func (s *ApiServer) Start(ctx context.Context) error {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", s.ping)
+	server := &http.Server{
+		Addr:    net.JoinHostPort(s.config.ApiServerHost, s.config.ApiServerPort),
+		Handler: mux,
+	}
+
+	go func() {
+		s.logger.Info("Listening on ", "port", s.config.ApiServerPort)
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			s.logger.Error("apiserver failed to listen and serve", "error", err)
+		}
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-ctx.Done()
+
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			s.logger.Error("apiserver failed to shutdown", "error", err)
+		}
+	}()
+
+	wg.Wait()
+
+	return server.ListenAndServe()
+}
+
+func (s *ApiServer) ping(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("pong"))
+	if err != nil {
+		log.Println("Failed to write in ping")
+	}
+}
