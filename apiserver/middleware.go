45bb6c0019eb37b4102aa32936003f61b9e00994
Ardit Zubaku <zubaku92@gmail.com>
2026-09-11 22:41:29 +0200

    Add auth middleware

--- apiserver/middleware.go
+++ apiserver/middleware.go
@@ -1,15 +1,89 @@
 package apiserver
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
+	"strings"
+	"uuid"
+
+	"github.com/ArditZubaku/async-api/store"
 )
+
+type userCtxKey struct{}
+
+func ContextWithUser(ctx context.Context, user *store.User) context.Context {
+	return context.WithValue(ctx, userCtxKey{}, user)
+}
 
 func newLoggingMiddleware(logger *slog.Logger) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			logger.Info("http request", "path", r.Method+" "+r.URL.Path)
 			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func newAuthMiddleware(jwtManager *JwtManager, userStore *store.UserStore, logger *slog.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasPrefix(r.URL.Path, "/auth") {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			authHeader := r.Header.Get("Authorization")
+			if authHeader == "" {
+				logger.Error("auth header is empty")
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			var token string
+			if parts := strings.SplitN(authHeader, " ", 2); len(parts) == 2 {
+				token = parts[1]
+			}
+
+			if token == "" {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			parsedToken, err := jwtManager.Parse(token)
+			if err != nil {
+				logger.Error("failed to parse token", "error", err)
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			if !jwtManager.IsAccessToken(parsedToken) {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			userIdStr, err := parsedToken.Claims.GetSubject()
+			if err != nil {
+				logger.Error("failed to extract user id from token subject", "error", err)
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			userId, err := uuid.Parse(userIdStr)
+			if err != nil {
+				logger.Error("user id is not valid uuid", "error", err)
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			user, err := userStore.ById(r.Context(), userId)
+			if err != nil {
+				logger.Error("failed to get user by id", "error", err)
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			next.ServeHTTP(w, r.WithContext(ContextWithUser(r.Context(), user)))
 		})
 	}
 }
