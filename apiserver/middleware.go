ae86130da076678e3ab33534544b3572f51f6515
Ardit Zubaku <zubaku92@gmail.com>
2026-09-13 16:15:28 +0200

    Add UserFromContext helper

--- apiserver/middleware.go
+++ apiserver/middleware.go
@@ -2,6 +2,7 @@ package apiserver
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 	"net/http"
 	"strings"
@@ -14,6 +15,15 @@ type userCtxKey struct{}
 
 func ContextWithUser(ctx context.Context, user *store.User) context.Context {
 	return context.WithValue(ctx, userCtxKey{}, user)
+}
+
+func UserFromContext(ctx context.Context) (*store.User, error) {
+	user, ok := ctx.Value(userCtxKey{}).(*store.User)
+	if !ok || user == nil {
+		return nil, errors.New("user not found in context")
+	}
+
+	return user, nil
 }
 
 func newLoggingMiddleware(logger *slog.Logger) func(next http.Handler) http.Handler {
